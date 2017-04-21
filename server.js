8d111dd9a64b20725b07a542a1def752a656315b
mladenlo <ptcmladen@hotmail.com>
2017-04-21 21:57:48 +0000

    second change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hellooooooooo');
+    res.send('Hellooo');
 });
 
 app.listen(8080, function(){
